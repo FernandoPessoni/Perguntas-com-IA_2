4b12feda1429e0ce27d8a34389ccc606a49024f4
FernandoPessoni <104445764+FernandoPessoni@users.noreply.github.com>
2024-09-04 08:05:27 -0300

    ok

---
+++ js/perguntas.js
@@ -0,0 +1,64 @@
+export const perguntas = [
+    {
+        enunciado: "Você é uma pessoa que sonha em entrar na NBA, joga há muitos anos e é muito dedicado, porém, tem apenas 1,59 de altura e seus amigos dizem que é impossível. O que você faz?",
+        alternativas: [
+            {
+                texto: "Sente dúvida de si mesmo.",
+                afirmacao: [
+                    "Devido as inseguranças que você mesmo criou, você para de treinar como antes e se torna um jogador inseguro."
+                    
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Não dá ouvidos a eles e continua treinando.",
+                afirmacao: [
+                    "Para compensar sua falta de altura, você foca em melhorar suas habilidades de drible, arremesso e defesa, focando em se tornar um jogador completo."
+                   
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Você estava jogando com seus amigos até que um olheiro da NBA veio até você e disse que você deveria fazer um teste para entrar no time dele. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Você aceita tentar, com confiança.",
+                afirmacao: [
+                    "Você faz o jogo para teste e surpreende o técnico com seu esforço e suas habilidades, e isso convence o técnico a te deixar entrar como titular."
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Você aceita tentar, mas com medo de falhar.",
+                afirmacao: [
+                    "Você não consegue jogar tão bem por causa do seu medo e insegurança, e o técnico acaba te deixando como um dos reservas."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Você encontra seus amigos depois de um tempo e te chamam para jogar uma partida de basquete com eles. Qual a sua resposta?",
+        alternativas: [
+            {
+                texto: "Vamos jogar",
+                afirmacao: [
+                    "Você joga com confiança e mostra tudo o que aprendeu com os treinos, provando que sua altura não importa."
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Melhor não",
+                afirmacao: [
+                    "Você não quer jogar pois fica inseguro de jogar mal mesmo estando em um time bom, e esse medo te limita em todos os jogos."
+                    
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+]
